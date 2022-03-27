77d94ae49cea74d30040d0aec4cb6603e64d7077
Kirill Chuvyzgalov <mr.chuvyzgalov@gmail.com>
2022-03-27 22:50:14 +0300

    add home_page

---
+++ lib/configuration/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const welcomeToColor = Color.fromRGBO(96, 96, 96, 1);
+  static const homePageOnboardingTextColor = Color.fromRGBO(128, 128, 128, 1);
+  static const homePageButtonColor = Color.fromRGBO(36, 36, 36, 1);
+  static const homePageButtonTextColor = Color.fromRGBO(255, 255, 255, 1);
+}
---
+++ lib/configuration/text_constants.dart
@@ -0,0 +1,6 @@
+class TextConstants {
+  static const welcomeToText = 'WELCOME TO';
+  static const homePageOnboardingText =
+      'The best simple place where you discover most wonderful computers and components';
+  static const homePageButtonText = 'Get Started';
+}
